420d42d3a8a63edb97d0c4ba2926df2a5b579f1a
Matis-Chedru <matisc972@gmail.com>
2026-07-15 00:01:22 +0200

    feat : ajout des getters et setters et de la methode getExtension

--- src/main/java/fr/smartmedia/modele/MediaFile.java
+++ src/main/java/fr/smartmedia/modele/MediaFile.java
@@ -2,6 +2,7 @@ package fr.smartmedia.modele;
 
 public abstract class MediaFile {
 
+
     private String originalPath;
     private String targetPath;
     private EnumerationEtat status;
@@ -10,4 +11,36 @@ public abstract class MediaFile {
         this.originalPath = originalPath;
         this.status = EnumerationEtat.EN_ATTENTE;
     }
+
+    public String getOriginalPath() {
+        return this.originalPath;
+    }
+
+    public String getTargetPath() {
+        return this.targetPath;
+    }
+
+    public void setTargetPath(String targetPath) {
+        this.targetPath = targetPath;
+    }
+
+    public EnumerationEtat getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnumerationEtat status) {
+        this.status = status;
+    }
+
+    //renvoie le texte qui ce situe apres le . d'un fichier
+    public String getExtension() {
+        int indexOfDot = this.originalPath.lastIndexOf('.');
+
+        if (indexOfDot == -1 || indexOfDot == this.originalPath.length() - 1) {
+            return "";
+        }
+
+        return this.originalPath.substring(indexOfDot + 1);
+    }
+
 }
